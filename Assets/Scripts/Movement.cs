9cb6ee861d5ff8597f30e6d296a651c606240083
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2018-10-15 09:23:49 +0200

    AddForce

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     public Rigidbody2D rBody;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//AddForce
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AddForce
         //.velocity
         //( MovePosition() )
 
         //AddTorque
         //.anuglarVelocity
         //
-	}
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rBody.AddForce(new Vector2(10f, 0f));
+    }
 }
