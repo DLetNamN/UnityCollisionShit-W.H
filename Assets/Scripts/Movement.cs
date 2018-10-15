905a56b57ac2c93f76057891bd27350d25ce5cf2
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2018-10-15 09:05:30 +0200

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//AddForce
+        //.velocity
+        //( MovePosition() )
+
+        //AddTorque
+        //.anuglarVelocity
+        //
+	}
+}
